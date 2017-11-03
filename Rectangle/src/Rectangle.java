54cb237767fb90a952fe87018e7ae3f8bcdfe9f6
songwell1024 <zysong0709@Foxmail.com>
2017-11-03 21:22:17 +0800

    first version

---
+++ Rectangle/src/Rectangle.java
@@ -0,0 +1,26 @@
+/**
+ * This program get the area of rectangle
+ * @version 1.0 2017.11.03
+ * @author songzhiyong
+ */
+public class Rectangle {
+	//public static double area;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Rectangletest rec = new Rectangletest(3,4);
+    //area = rec.getArea();
+    System.out.print(rec.getArea());
+	}
+}
+class Rectangletest{
+  private double width;
+  private double height;
+  
+  public Rectangletest(double wid,double hei ){
+	  this.width = wid;
+	  this.height = hei;
+  }
+  public double getArea(){
+	  return width*height;
+  }
+}
